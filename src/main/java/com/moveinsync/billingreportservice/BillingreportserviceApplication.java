40c121d9519ab1fd784ba50b668e065aa8b456bd
Adi <adityendra.pratap@moveinsync.com>
2024-02-26 17:35:54 +0530

    Initial commit

---
+++ src/main/java/com/moveinsync/billingreportservice/BillingreportserviceApplication.java
@@ -0,0 +1,13 @@
+package com.moveinsync.billingreportservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BillingreportserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BillingreportserviceApplication.class, args);
+	}
+
+}
