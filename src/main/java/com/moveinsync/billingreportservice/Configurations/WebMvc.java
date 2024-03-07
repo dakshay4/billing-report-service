b8560d1d6d26485e8d7df22d0b4bbfb48eb22c4c
dakshay <dakshay.agarwal@moveinsync.com>
2024-03-07 16:27:18 +0530

    feature/TB-25330: allowed cors

--- src/main/java/com/moveinsync/billingreportservice/Configurations/WebMvc.java
+++ src/main/java/com/moveinsync/billingreportservice/Configurations/WebMvc.java
@@ -2,6 +2,7 @@ package com.moveinsync.billingreportservice.Configurations;
 
 import com.moveinsync.billingreportservice.controllers.CustomInterceptor;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,6 +12,11 @@ public class WebMvc implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new CustomInterceptor());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*").allowedMethods("PUT", "DELETE", "GET", "PATCH", "POST");
     }
 
 }
